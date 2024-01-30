22fe6a0fa66149fbc7da571ca8dbb96c72f49bc4
Michael Hampel <michham@m08990-01.local>
2024-01-30 16:14:43 +0100

    enode rest client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
+
+use reqwest::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     println!("Hello, world!");
+
+    let client = reqwest::Client::builder().build()?;
+
+    let res = client
+        .get("http://httpbin.org/get")
+        .send()
+        .await?;
+    
+    println!("Http Status: {}", res.status());
+    println!("Headers: \n{:#?}", res.headers());
+
+    let body = res.text().await?;
+    println!("Response body: \n{}", body);
+
+    Ok(())
+
 }
